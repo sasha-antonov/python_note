d2c90108e93ea1d518bfd7f97e1b680c3305bd42
sasha-antonov <sasha.antonov1989@gmail.com>
2014-12-11 14:32:12 +0400

    Revert "python note"
    This reverts commit 7ea84f9cadfda6492062b338057bd7459347a596.

--- Ctypes_C_DLL/ctypes_C_dll.py
+++
@@ -1,3 +0,0 @@
-from ctypes import *
-mydll = cdll.LoadLibrary("C:\Python27\UserProj\dll.dll")
-print mydll.Func(100)
--- PySerial COMport.py
+++
@@ -1,9 +0,0 @@
-import serial
-
-ser = serial.Serial('COM1', 9600, dsrdtr = 0, timeout = 1)
-# timeout - если соединение не установится, то через 1 сек. произойдет разъединение
-# dsr/dtr (готовность источника/приемника) выполнена аппаратно
-
-ser.write(b'\xFF')
-input_data = ser.read()
-ser.close
